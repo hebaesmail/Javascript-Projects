5cc6071f2d6fd33e0e25350ba434b2e9fc56bf14
hebaesmail <hebaesmail0@gmail.com>
2022-04-15 19:11:09 -0500

    JS Practice

---
+++ Project1-expressions-alert/JavaScript/project1.js
@@ -0,0 +1,15 @@
+var A = "This is a string"; 
+document.write(A);
+
+var B = "Good Mornin'!"; //pop-up alert box
+window.alert(B);
+
+document.write("\"Smile and Enjoy the world around you," + " because life is short and these little moments will be forever gone.\"" + "-Unknown");
+
+var C = "Concatenated" + " String"
+document.write(C), fontcolor(green);
+
+var Family = "The Smiths", Dad = "Mark", Mom = "Sally", Daughter = "Nina", Son = "Derek"; //Group Variable
+document.write(Son);
+
+document.write(2+6); //Expression
