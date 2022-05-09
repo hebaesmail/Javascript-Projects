bbc7d851a9bff058ad698f3b379619d1b34c7ba1
hebaesmail <hebaesmail0@gmail.com>
2022-05-09 18:10:24 -0500

    project 1

--- Project1-expressions-alert/JavaScript/project1.js
+++ Project1-expressions-alert/JavaScript/project1.js
@@ -1,19 +1,14 @@
-var A = "This is a string."; 
-document.write(A)
+var A = " Today is a beautiful day."; 
+
+var C = " Perfect for a picnic.";
 
 var B = "Good Mornin'!"; //pop-up alert box
 window.alert(B);
 
-var Z = (`\"Quote`); 
+var Z = (`\"Quote"`); 
 document.write(Z);
 
-div = document.getElementById("container");
-div.fontcolor("red");
-
-var C = "This is another string.";
-document.write(C);
-
-document.write("concatenate + var A + var C");
+document.write(A+C);
 
 var Family = "The Smiths", Dad = "Mark", Mom = "Sally", Daughter = "Nina", Son = "Derek"; //Group Variable
 document.write(Son);
