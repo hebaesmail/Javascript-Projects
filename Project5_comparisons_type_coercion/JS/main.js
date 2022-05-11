d13875b31eee5d3015bdf19c02fbce269064d021
hebaesmail <hebaesmail0@gmail.com>
2022-05-11 18:26:21 -0500

    P5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,19 @@
+document.write("15"+ 6);
+
+document.write(17 > 12);
+
+document.write(12 < 65);
+
+console.log(76<73);
+
+document.write(12 == 65); // Equals statement
+
+document.write(5>2 && 10>4); //AND statement
+
+document.write(56>54 || 23>57);
+
+document.write(12>10 === 12>9);
+
+function not_function() {
+    document.getElementById("Not") .innerHTML = ! (20>10); //makes the statement false no matter the data
+}
