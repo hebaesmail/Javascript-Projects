38c444e36e5a3dd0e9e1be6338b3b4cb33d23519
hebaesmail <hebaesmail0@gmail.com>
2022-05-24 17:33:59 -0500

    P9 Merge

--- Project9_Countdown_Slideshow/JS/main.js
+++ Project9_Countdown_Slideshow/JS/main.js
@@ -1,3 +1,21 @@
+function countdown() {
+  var seconds = document.getElementById("seconds").value;
+
+  function tick() {
+      seconds = seconds - 1;
+      timer.innerhtml = seconds;
+      var time = setTimeout(tick, 1000);
+      if (seconds == -1) {
+          alert("Times Up!");
+          clearTimeout(time);
+          timer.innerhtml = "";
+      }
+  }
+  tick();
+}
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
