08ab1122781852338e9822f2072f92d08cf926f1
hebaesmail <hebaesmail0@gmail.com>
2022-05-24 15:44:13 -0500

    P8

--- Project8_string_methods/main.js
+++ Project8_string_methods/main.js
@@ -3,17 +3,22 @@ function full_sentence() {
     var part_2 = "made this ";
     var part_3 = "into a complete ";
     var part_4 = "sentence. ";
-    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    var whole_sentence = part_1.concat(part_2, part_3, part_4); //Formulates strings into a full message
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 
 function slice_method() {
     var Sentence = "All work and no play has makes Johnny a dull boy";
     var Section = Sentence.slice(27,33);
-    document.getElementById("Slice") .innerHTML = Section;
+    document.getElementById("Slice") .innerHTML = Section; //Splits a sentence into parts
 }
 
 function string_Method() {
     var X =182;
     document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method () {
+    var x = 12938.573948373;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
 }
