64d4353b75a529f61a17c661f1c73ff4dda69420
hebaesmail <hebaesmail0@gmail.com>
2022-05-24 15:28:16 -0500

    P6

--- Project6_ternary__operators_constructors/JS/JS/main.js
+++ Project6_ternary__operators_constructors/JS/JS/main.js
@@ -1,8 +1,8 @@
 function Vote_Function() {
     var Age, Can_vote;
-    Age = document.getElementById("Age") .value;
+    Age = document.getElementById("Age").value;
     Can_vote = (Age < 18) ? "You are not eligible" : "You are eligible";
-    document.getElementById("Vote") .innerHTML = Can_vote + " to vote.";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
 
 function Vehicle(Make, Model, Year, Color) {
@@ -16,15 +16,15 @@ var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep","Trail Hawk",2019 , "White and Black");
 var Erik = new Vehicle("Ford","Pinto", 1991, "Mustard");
 function myFunction() {
-    document.getElementById("Keywords_and_Constructors") .innerHTML = "Erik drives a " + 
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + 
     Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
 
 function count_function() {
-    document.getElementById("nested_function") .innerHTML = Count();
+    document.getElementById("nested_function").innerHTML = Count();
     function Count() {
         var Starting_point = 4;
-        function Plus_one() (Starting_point += 1;)
+        function Plus_one() {Starting_point += 1;}
         Plus_one();
         return Starting_point;
     }
