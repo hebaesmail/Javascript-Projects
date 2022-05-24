b98736d9cbb95e1b7b5ac50051f342785dc71717
hebaesmail <hebaesmail0@gmail.com>
2022-05-24 14:56:19 -0500

    P8

---
+++ Project8_string_methods/main.js
@@ -0,0 +1,16 @@
+function full_sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+function slice_method() {
+    var Sentence = "All work and no play has makes Johnny a dull boy";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice") .innerHTML = Section;
+}
+
+function 
